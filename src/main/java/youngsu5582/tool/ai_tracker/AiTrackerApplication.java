3b59c5370eecd3b487b29b6b0b2b75997a6afa42
youngsu5582 <98307410+youngsu5582@users.noreply.github.com>
2025-07-19 20:53:34 +0900

    init

---
+++ src/main/java/youngsu5582/tool/ai_tracker/AiTrackerApplication.java
@@ -0,0 +1,13 @@
+package youngsu5582.tool.ai_tracker;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AiTrackerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(AiTrackerApplication.class, args);
+    }
+
+}
