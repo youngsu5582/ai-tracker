320dc083701cbbc647d12271fe2f337297ac85ed
youngsu5582 <98307410+youngsu5582@users.noreply.github.com>
2025-09-04 00:05:47 +0900

    HOTFIX: ⚠️BACKEND CODE RESET!!⚠️
    more specify requirements
    focused on my learning

--- backend/src/main/java/youngsu5582/tool/ai_tracker/AiTrackerApplication.java
+++ backend/src/main/java/youngsu5582/tool/ai_tracker/AiTrackerApplication.java
@@ -2,10 +2,8 @@ package youngsu5582.tool.ai_tracker;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.scheduling.annotation.EnableAsync;
 
 @SpringBootApplication
-@EnableAsync
 public class AiTrackerApplication {
 
     public static void main(String[] args) {
