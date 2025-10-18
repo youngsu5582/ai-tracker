c1cff1733ee61d043c7355f4bdc813bbd2ed2a53
youngsu5582 <98307410+youngsu5582@users.noreply.github.com>
2025-10-19 01:14:45 +0900

    feat: 프롬프트 제공자 enum alias 로 저장되게 변경

--- backend/src/main/java/youngsu5582/tool/ai_tracker/domain/prompt/PromptProvider.java
+++ backend/src/main/java/youngsu5582/tool/ai_tracker/domain/prompt/PromptProvider.java
@@ -1,5 +1,16 @@
 package youngsu5582.tool.ai_tracker.domain.prompt;
 
+import com.fasterxml.jackson.annotation.JsonValue;
+import lombok.Getter;
+
+@Getter
 public enum PromptProvider {
-    OPEN_AI
+    OPEN_AI("open-ai");
+
+    @JsonValue
+    private final String alias;
+
+    PromptProvider(String alias) {
+        this.alias = alias;
+    }
 }
